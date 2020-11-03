acac9c3996518a4784be07ba9e98dc74cae0c781
IndyJoe <IndyJoeBRJS@gmail.com>
2020-11-03 15:37:13 -0500

    privacy policy, about, contact us complete

--- src/sections/ContactUs.js
+++ src/sections/ContactUs.js
@@ -1,21 +1,24 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input, textarea } from 'reactstrap';
+import '../styles/ContactUs.css';
 
 const ContactUs = (props) => {
 
     return (
-        <div>
+        <div className="contactForm">
+          <h2> -  Contact Us  -</h2>
             <Form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xnqovbrl" method="post">
                 <FormGroup id="fs-frm-inputs">
-                    <Label for="full-name">Full Name</Label>
+                    <Label for="full-name" className="formLabels">Full Name</Label>
                     <Input type="text" name="name" id="full-name" placeholder="First and Last" required="" />
-                    <Label for="email-address">Email Address</Label>
+                    <Label for="email-address" className="formLabels">Email Address</Label>
                     <Input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="" />
-                    <Label for="message">Message</Label>
-                    <textarea rows="5" name="message" id="message" placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus." required="" />
+                    <Label for="message" className="formLabels">Message</Label>
+                    <br/>
+                    <textarea rows="5" cols="60" name="message" id="message" placeholder="Thoughts, Questions, Suggestions or Concerns?" required="" />
                     <Input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
                 </FormGroup>
-                <Input type="submit" value="Submit" />
+                <Input type="submit" value="Submit" id="inputButton"/>
             </Form>
         </div>
     )
--- src/sections/PrivacyPolicy.js
+++ src/sections/PrivacyPolicy.js
@@ -5,19 +5,14 @@ const PrivacyPolicy = (props) => {
 
     return (
         <div className="privacyPolicy">
-            <h3>Privacy Policy of https://heroku.oursite.org</h3>
+            <h3 className="title">Privacy Policy of https://heroku.oursite.org</h3>
             <p>At Personary, we collect and manage user data according to the following Privacy Policy.</p>
             
             <h4>Data Collected</h4>
-            <p>We collect information you provide directly to us. For example, we collect information when you create an account, subscribe, participate in any interactive features of our services, fill out a form, request customer support or otherwise communicate with us. The types of information we may collect include your name, email address, postal address, credit card information and other contact or identifying information you choose to provide.</p>
-
-            <p>We collect anonymous data from every visitor of the Website to monitor traffic and fix bugs. For example, we collect information like web requests, the data sent in response to such requests, the Internet Protocol address, the browser type, the browser language, and a timestamp for the request.</p>
-
-            <p>We also use various technologies to collect information, and this may include sending cookies to your computer. Cookies are small data files stored on your hard drive or in your device memory that helps us to improve our services and your experience, see which areas and features of our services are popular and count visits. We may also collect information using web beacons (also known as "tracking pixels"). Web beacons are electronic images that may be used in our services or emails and to track count visits or understand usage and campaign effectiveness. Our Privacy Policy was created with the help of the Privacy Policy Template and the Generate Privacy Policy Generator.</p>
-
+            <p>We only collect information you provide directly to us. The types of information we may collect include your name, email address and other identifying information you choose to provide.</p>
 
             <h4>Use of the Data</h4>
-            <p>We only use your personal information to provide you the Personary services or to communicate with you about the Website or the services.</p>
+            <p>We only use your personal information to provide you the Personary services or to communicate with you about the Website.</p>
 
             <p>We employ industry standard techniques to protect against unauthorized access of data about you that we store, including personal information.</p>
 
@@ -30,20 +25,13 @@ const PrivacyPolicy = (props) => {
             </ul>
 
             <h4>Sharing of Data</h4>
-            <p>We don't share your personal information with third parties. Aggregated, anonymized data is periodically transmitted to external services to help us improve the Website and service.</p>
-
-            <p>We may allow third parties to provide analytics services. These third parties may use cookies, web beacons and other technologies to collect information about your use of the services and other websites, including your IP address, web browser, pages viewed, time spent on pages, links clicked and conversion information.</p>
-
-            <p>We also use social buttons provided by services like Twitter, Google+, LinkedIn and Facebook. Your use of these third party services is entirely optional. We are not responsible for the privacy policies and/or practices of these third party services, and you are responsible for reading and understanding those third party services' privacy policies.</p>
-
+            <p>We do not share your personal information with third parties.</p>
         
             <h4>Cookies</h4>
-            <p>We may use cookies on our site to remember your preferences.</p>
-
-            <p>For more general information on cookies, please read "What Are Cookies".</p>
+            <p>We do not use cookies on our site to remember your preferences.</p>
 
             <h4>Opt-Out, Communication Preferences</h4>
-            <p>You may modify your communication preferences and/or opt-out from specific communications at any time. Please specify and adjust your preferences.</p>
+            <p>You may not modify your communication preferences and/or opt-out from specific communcations as we will only communicate with you under one of the circumstances as outlined in 'Use of the Data'.</p>
         
             <h4>Security</h4>
             <p>We take reasonable steps to protect personally identifiable information from loss, misuse, and unauthorized access, disclosure, alteration, or destruction. But, you should keep in mind that no Internet transmission is ever completely secure or error-free. In particular, email sent to or from the Sites may not be secure.</p>
