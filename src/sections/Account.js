1e37f096c2eacd65d8301c2d9aceb0276c22aacf
IndyJoe <IndyJoeBRJS@gmail.com>
2020-11-04 15:14:08 -0500

    slight change to My Account

--- src/sections/Account.js
+++ src/sections/Account.js
@@ -13,21 +13,21 @@ const Account = (props) => {
 
     
     return (
-    <div>
+        <div>
         
-        <div className="profilePicture"><UpdateProfileImage  userimg={accountInfo.url_userimage}/><br/></div>
-            <div className="mainAcctDiv">
-            <div className="accountHeader">My Account</div>
-            <div className="userName">{accountInfo.username}</div>
-            <div className="password">Password:   ****************</div>
-            <div className="changePassword">{ showPasswordModal ? <ChangePasswordModal /> : <></> }</div>
-            <div className="dateJoined">Date Joined: {accountInfo.createdAt}</div>
-            <div className="deleteAcct"><DeleteAccount /></div>
-        </div>
-        </div>
-        
-    
+            <div className="profilePicture"><UpdateProfileImage  userimg={accountInfo.url_userimage}/><br/></div>
+                <div className="mainAcctDiv">
+                <div className="accountHeader">- My Account -</div>
+                <div className="userName">{accountInfo.username}</div>
+                <div className="password">Password:   ****************</div>
+                <div className="changePassword">{ showPasswordModal ? <ChangePasswordModal /> : <></> }</div>
+                <div className="dateJoined">Date Joined: {accountInfo.createdAt}</div>
+                <div className="deleteAcct"><DeleteAccount /></div>
+            </div>
+
+        </div> 
     )
+
 };
 
 export default Account;
